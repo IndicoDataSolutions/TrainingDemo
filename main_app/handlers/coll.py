4cf9fb5926da9f466f8fb21b1e9304a8cb20a91e
Slater Victoroff <slater@Slaters-MacBook-Pro.local>
2016-05-29 17:06:02 -0400

    Search working, 3 buttons left to MVP. 2 easy 1 hard

--- main_app/handlers/coll.py
+++ main_app/handlers/coll.py
@@ -1,4 +1,7 @@
+import json
+
 import numpy as np
+import requests
 from tornado.options import options
 from indicoio.custom import Collection
 
@@ -14,14 +17,42 @@ class DeleteCollectionHandler(BaseHandler):
 		self.redirect("/")
 
 class ViewCollectionHandler(BaseHandler):
-	def get(self, data_type, collection_name):
+
+	def _details(self, data_type, collection_name):
+		"""
+		Helper function
+		"""
 		coll = Collection(collection_name)
 		labels = []
 		if data_type == "text":
 			labels = coll.predict("a").keys()
 		elif data_type == "image":
 			labels = coll.predict(np.array([[0]])).keys()
+		return labels, coll
+
+	def get(self, data_type, collection_name):
+		labels, coll = self._details(data_type, collection_name)
 		return self.render("view.html", labels=labels, collection=coll)
+
+	def post(self, data_type, collection_name):
+		labels, coll = self._details(data_type, collection_name)
+		search = self.get_argument("query")
+		search_payload = {
+			"key": options.pixabay_key,
+			"q": search,
+			"safesearch": True,
+			"per_page": 100,
+		}
+		results = json.loads(requests.get("https://pixabay.com/api", params=search_payload).content)["hits"]
+		search_results = [{"preview": result['previewURL'], "base": result["webformatURL"]} for result in results]
+
+		self.render(
+			"view.html",
+			labels = labels,
+			collection = coll,
+			search_results = search_results,
+			previous_search = search
+		)
 
 # class CollectionSearchHandler(BaseHandler):
 # 	def 
